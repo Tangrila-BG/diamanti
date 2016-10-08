a69a90ff9eae55a5cc207c5948639cb4679cd190
Tangrila-BG <phatomiki@gmail.com>
2016-10-08 19:29:47 +0300

    added images to use and sample code of loading them

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,5 +1,61 @@
 /**
  * Created by tangrila on 08-Oct-16.
  */
+
+//
 // This is where the game code will be written
 // Every function from frame.js is accessible here.
+//
+
+//
+// global vars:
+// canvas = document.getElementById("canvas");
+// ctx = canvas.getContext("2d");
+//
+
+//
+// Images dimensions
+// WIDTH x HEIGHT
+// bira.jpg - 45x110 ; rakia.jpg - 35x110 ;
+// salata.jpg - 130x110 ; shopska-salata.gif - 119x100
+// nakov.jpg - 100 x 100
+//
+
+function game() {
+	/*
+	// A Way of loading the images
+	let nakov = new Image();
+	let salataShopska = new Image();
+	let biraImg = new Image();
+	let rakiaImg = new Image();
+	let salata = new Image();
+
+	// catcher
+	nakov.onload = function () {
+		ctx.drawImage(nakov, 350, 500);
+	};
+	// salads
+	salataShopska.onload = function () {
+		ctx.drawImage(salataShopska, 0, 0);
+	};
+	salata.onload = function () {
+		ctx.drawImage(salata, 350, 0);
+	};
+	// alcohol
+	biraImg.onload = function () {
+		ctx.drawImage(biraImg, 150, 0);
+	};
+	rakiaImg.onload = function () {
+		ctx.drawImage(rakiaImg, 250, 0);
+	};
+
+	// load image
+	biraImg.src = 'images/bira.jpg';
+	rakiaImg.src = 'images/rakia.jpg';
+	salataShopska.src = 'images/shopska-salata.gif';
+	salata.src = 'images/salata.jpg';
+	nakov.src = 'images/nakov.jpg'
+	*/
+}
+
+game();
