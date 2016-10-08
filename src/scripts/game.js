ccd5750ffc2552a0a71db203bb1d5961d4690a89
Tangrila-BG <phatomiki@gmail.com>
2016-10-08 16:51:40 +0300

    fixed comment typo

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -2,4 +2,4 @@
  * Created by tangrila on 08-Oct-16.
  */
 // This is where the game code will be written
-// Every from frame.js is accessible here.
+// Every function from frame.js is accessible here.
